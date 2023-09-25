da99e9fd6afac536966fd2aa394cd96cce92395e
Gabriel Ramos <117670546+ramosantos@users.noreply.github.com>
2023-09-25 14:10:23 +0000

    trying

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
-import "./style.css";
 import Home from "./pages/Home.vue";
-import Login from './pages/Login.vue';
+import Login from "./pages/Login.vue";
+import "./style.css";
 
-const app = createApp(Home);
+// Create the Vue app instance
+const vueApp = createApp(Home);
 
-app.mount("#app");
+// Mount the app to the element with ID 'app'
+vueApp.mount("#app");
 
+// Create the router
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      {
-        path: "/login",
-        component: Login,
-      }
-    ],
-  });
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/login",
+      component: Login,
+    },
+    {
+      path: "/",
+      component: Home,
+    },
+  ],
+});
 
-app.use(router);
+// Use the router in the app
+vueApp.use(router);
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,5 @@
 <script setup></script>
 
 <template>
-    <h1>Hello</h1>
+  <h1>login</h1>
 </template>
